a10b8ae9e75fc600808f63be8c76873af63b2c75
DOUGBOWMER <dougbowmer3@gmail.com>
2021-09-28 18:46:56 -0400

    git commit

---
+++ Basic JavaScript Projects/Project5_comparisons_type_coercion/JS/main.js
@@ -0,0 +1,10 @@
+function writetype() {
+var four = document.write(typeof 4);
+document.getElementById("answer").innerHTML= four;
+} 
+
+function coer() {
+    var ten = "ten"
+    var two= "2"
+    document.getElementById("answer2").innerHTML= ten + two
+}
---
+++ NAN Challenge/JS/example.js
@@ -0,0 +1,13 @@
+function test() {
+   
+    document.getElementById("tester").innerHTML= 0/0;
+}
+
+function test2() {
+    document.getElementById("tester2").innerHTML= isNaN('Dog');
+}
+
+function test3() {
+    document.getElementById("tester3").innerHTML= isNaN('200500');
+
+} 
