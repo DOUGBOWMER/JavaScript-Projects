eb77e182e5f570530caeb021ebd0e717734aae98
DOUGBOWMER <dougbowmer3@gmail.com>
2021-09-29 22:22:54 -0400

    commit

---
+++ Basic JavaScript Projects/Project7_scope_time_function/JS/main.js
@@ -0,0 +1,10 @@
+var x = 30
+
+function Addx() {
+    var tracktime = 2 * x;
+    document.getElementById("frosty").innerhtml= tracktime;
+}
+function reach() {
+    var finishtime= tracktime-x;
+    document.getElementById("snowman").innerhtml= finishtime
+}
