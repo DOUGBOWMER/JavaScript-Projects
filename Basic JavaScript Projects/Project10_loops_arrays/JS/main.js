88e4b09f9f5f4269a8db1ad4170bbb114ec17091
DOUGBOWMER <dougbowmer3@gmail.com>
2021-10-03 07:03:48 -0400

    string challenge

---
+++ Basic JavaScript Projects/Project10_loops_arrays/JS/main.js
@@ -0,0 +1,11 @@
+
+function cloop() {
+    var Digit = "";
+    var x = 1;
+    while( x < 11) {
+        Digit += "<br>" + x;
+        x++;
+     }
+     document.getElementById("loop").innerHTML= "All Done"
+};
+
---
+++ LENGTH PROPERTY CHALLENGE/JS/main.js
@@ -0,0 +1,4 @@
+function charl() {
+    let str = "hello world"
+     document.getElementById("hw") = str.length
+ }
