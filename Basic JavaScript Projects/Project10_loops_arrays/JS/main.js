ac83a573d2147aadf2c0e51944a5dbb368ef4f76
DOUGBOWMER <dougbowmer3@gmail.com>
2021-10-03 21:56:17 -0400

    commit

--- Basic JavaScript Projects/Project10_loops_arrays/JS/main.js
+++ Basic JavaScript Projects/Project10_loops_arrays/JS/main.js
@@ -1,5 +1,5 @@
 
-function cloop() {
+function countloop() {
     var Digit = "";
     var x = 1;
     while( x < 11) {
@@ -9,3 +9,12 @@ function cloop() {
      document.getElementById("loop").innerHTML= "All Done"
 };
 
+var Instruments = ["Guitar", "Drums", "Piano", "Violin", "Trumpet", "Flute"];
+var Content = "";
+var Y; 
+function for_Loop(){
+    for (Y= 0; Y < Instruments.lenght; Y++) {
+        Content += Instruments[Y] + "<br>";
+        }
+        document.getElementById("List_of_Instruments").innerHTML = Content; 
+}
