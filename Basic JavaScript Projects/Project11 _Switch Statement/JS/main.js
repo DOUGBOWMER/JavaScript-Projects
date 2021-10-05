936a3be8d44458bef0f84da38cf829a3ca5029e6
DOUGBOWMER <dougbowmer3@gmail.com>
2021-10-05 02:49:02 -0400

    commit

---
+++ Basic JavaScript Projects/Project11 _Switch Statement/JS/main.js
@@ -0,0 +1,28 @@
+function color_Fn() {
+    var Color_Output;
+    var Colors = document.getElementById("ColorIn").value;
+    var Color_String = " is a great color!";
+    switch(Colors) {
+        case "Red":
+                Color_Output = "Red" + Color_String;
+            break;
+            case "Yellow":
+                Color_Output = "Yellow" + Color_String;
+            break;
+            case "Green":
+                Color_Output = "Green" + Color_String;
+            break;
+            case "Blue":
+                Color_Output = "Blue" + Color_String;
+            break;
+            case "Pink":
+                Color_Output = "Pink" + Color_String;
+            break;
+            case "Purple":
+                Color_Output = "Purple" + Color_String;
+            break;
+            default:
+                Color_Output = "Please enter a color exactly as written on the above list";    
+            }
+            document.getElementById("Output").innerHTML = Color_Output;
+        }
