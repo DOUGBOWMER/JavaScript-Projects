db79b70ca04500b10df259b8b9ca0d524671eb62
DOUGBOWMER <dougbowmer3@gmail.com>
2021-10-07 00:41:02 -0400

    HTML 3 chanlenge commit

---
+++ JavaScript Projects/JavaScript/JavaScript_3.js
@@ -0,0 +1,4 @@
+function displayType(character) {
+    var characterType = character.getAttribute("data-moto-type");
+    alert(characterType + " is the best " + character.innerHTML + " motorcycle they make!");
+}
